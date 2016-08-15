f424d59ea35bc3e8f2be4f4e7ebe287a13fc9a1d
crashtack <crashtack@gmail.com>
2016-08-15 15:57:46 -0700

    created tox.ini, setup.py, and scr/ files

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="http-server",
+    description="A Python implementation of a simple http echo client/server.",
+    version=0.1,
+    author="David Banks, Derek Hewitt",
+    author_email="crashtack@gmail.com, derekmhewitt@gmail.com",
+    license='MIT',
+    py_modules=['client', 'server'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
+)
