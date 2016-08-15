e9b87775ff819c81f856614fa73ebb146416b9f7
crashtack <crashtack@gmail.com>
2016-08-15 16:28:00 -0700

    server prints out message from client

--- src/client.py
+++ src/client.py
@@ -3,14 +3,14 @@ import socket
 
 
 def client():
-    infos = socket.getsddrinfo('127.0.0.1', 5001)
+    infos = socket.getaddrinfo('127.0.0.1', 5001)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
 
     echo_client = socket.socket(*stream_info[:3])
     echo_client.connect(stream_info[-1])
 
     message = u'Hello over there in the other window'
-    client.sendall(message.encode('utf8'))
+    echo_client.sendall(message.encode('utf8'))
 
 
 
