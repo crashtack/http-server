0a7bcc7590c8768d5ff642be04e1e9b8ef18d699
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-15 20:36:03 -0700

    client/server finished, tests written, two still failing for ascii characters

--- src/test_server.py
+++ src/test_server.py
@@ -9,89 +9,46 @@ test needed.
 4. messages containing non-ascii chars
 """
 import pytest
+from client import client_send
 
 TEST_SHORT_BUFFERS = [
     ("1234", "1234"),
     ("string", "string"),
     ("13 Elms", "13 Elms")
 ]
-
 TEST_LONG_BUFFERS = [
     ("thisisalongstring", "thisisalongstring"),
     ("thisisalongerstringwoooo", "thisisalongerstringwoooo")
 ]
-
 TEST_BUFFER_MULTIPLES = [
     ("12345678", "12345678"),
     ("1234567812345678", "1234567812345678")
 ]
-
 TEST_NON_ASCII = [
     ("çç√√", "çç√√"),
     ("´©˙π", "´©˙π")
 ]
 
 
-def test_set_up():
-    """Set up server for testing purposes."""
-    from server import server
-    server()
-
-
-def test_tear_down():
-    """Tear down testing server."""
-    from server import server
-    server.shutdown(1)
-    server.close()
-
-
 @pytest.mark.parametrize('input, output', TEST_SHORT_BUFFERS)
 def test_short_buffers(input, output):
-    """Summary.
-
-    Returns:
-        TYPE: Description
-    """
-    from client import client
-    test_set_up()
-    assert client(input) == output
-    test_tear_down()
+    """Test client_send with short test data."""
+    assert client_send(input) == output
 
 
 @pytest.mark.parametrize('input, output', TEST_LONG_BUFFERS)
 def test_long_buffers(input, output):
-    """Summary.
-
-    Returns:
-        TYPE: Description
-    """
-    from client import client
-    test_set_up()
-    assert client(input) == output
-    test_tear_down()
+    """Test client_send with long test data."""
+    assert client_send(input) == output
 
 
 @pytest.mark.parametrize('input, output', TEST_BUFFER_MULTIPLES)
 def test_buffer_multiples(input, output):
-    """Summary.
-
-    Returns:
-        TYPE: Description
-    """
-    from client import client
-    test_set_up()
-    assert client(input) == output
-    test_tear_down()
+    """Test client_send with multiples of buffer length."""
+    assert client_send(input) == output
 
 
 @pytest.mark.parametrize('input, output', TEST_NON_ASCII)
 def test_non_ascii(input, output):
-    """Summary.
-
-    Returns:
-        TYPE: Description
-    """
-    from client import client
-    test_set_up()
-    assert client(input) == output
-    test_tear_down()
+    """Test client_send with non-ascii characters."""
+    assert client_send(input) == output
