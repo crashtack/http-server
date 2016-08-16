1dc1bce8f46e35db3cc10a23070a2649cc989342
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-15 17:04:17 -0700

    basic server up and running, functional tests are next

--- src/client.py
+++ src/client.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Summary."""
 import socket
 
 
 def client():
-    infos = socket.getaddrinfo('127.0.0.1', 5001)
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    infos = socket.getaddrinfo('127.0.0.1', 5002)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
 
     echo_client = socket.socket(*stream_info[:3])
@@ -12,10 +18,21 @@ def client():
     message = u'Hello over there in the other window'
     echo_client.sendall(message.encode('utf8'))
 
+    # echo_client.listen(2)
+    # conn, addr = echo_client.accept()
 
-
-
-
+    message = ""
+    buffer_length = 8
+    return_message_complete = False
+    while not return_message_complete:
+        part = echo_client.recv(buffer_length)
+        message += (part.decode('utf8'))
+        if len(part) < buffer_length:
+            print(str(message))
+            print("I like boats")
+            # conn.sendall(message.encode('utf8'))
+            echo_client.close()
+            return_message_complete = True
 
 if __name__ == '__main__':
     client()
--- src/server.py
+++ src/server.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
+"""Summary."""
 import socket
 
 
 def server():
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5001)
+    address = ('127.0.0.1', 5002)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
 
+    message = ""
     buffer_length = 8
     message_complete = False
     while not message_complete:
         part = conn.recv(buffer_length)
-        print(part.decode('utf8'))
+        message += (part.decode('utf8'))
         if len(part) < buffer_length:
+            print(str(message))
+            print("I like boats")
+            conn.sendall(message.encode('utf8'))
+            server.close()
             message_complete = True
 
 
