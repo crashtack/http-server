d127686676f0dce953ce143ef3eeeca42736af59
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-15 20:11:16 -0700

    tests written, testing and adjustments soon"

--- src/client.py
+++ src/client.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """Summary."""
 import socket
+import sys
 
 
-def client():
+def client_send(msg):
     """Summary.
 
     Returns:
@@ -12,27 +13,22 @@ def client():
     infos = socket.getaddrinfo('127.0.0.1', 5002)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
 
-    echo_client = socket.socket(*stream_info[:3])
-    echo_client.connect(stream_info[-1])
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
 
-    message = u'Hello over there in the other window'
-    echo_client.sendall(message.encode('utf8'))
+    client.sendall(msg.encode('utf8'))
+    client.shutdown(1)
 
-    # echo_client.listen(2)
-    # conn, addr = echo_client.accept()
-
-    message = ""
+    message_2 = ""
     buffer_length = 8
     return_message_complete = False
     while not return_message_complete:
-        part = echo_client.recv(buffer_length)
-        message += (part.decode('utf8'))
+        part = client.recv(buffer_length)
+        message_2 += part.decode('utf8')
         if len(part) < buffer_length:
-            print(str(message))
-            print("I like boats")
-            # conn.sendall(message.encode('utf8'))
-            echo_client.close()
             return_message_complete = True
+    client.close()
+    return message_2
 
 if __name__ == '__main__':
-    client()
+    print(client_send(sys.argv[1]))
--- src/test_server.py
+++ src/test_server.py
@@ -1,2 +1,97 @@
 # -*- coding: utf-8 -*-
-"""Summary."""
+"""Summary.
+
+test needed.
+
+1. messages shorter than 1 buffer length
+2. message longer than several buffer lengths
+3. messages that are multiples of 1 buffer length
+4. messages containing non-ascii chars
+"""
+import pytest
+
+TEST_SHORT_BUFFERS = [
+    ("1234", "1234"),
+    ("string", "string"),
+    ("13 Elms", "13 Elms")
+]
+
+TEST_LONG_BUFFERS = [
+    ("thisisalongstring", "thisisalongstring"),
+    ("thisisalongerstringwoooo", "thisisalongerstringwoooo")
+]
+
+TEST_BUFFER_MULTIPLES = [
+    ("12345678", "12345678"),
+    ("1234567812345678", "1234567812345678")
+]
+
+TEST_NON_ASCII = [
+    ("çç√√", "çç√√"),
+    ("´©˙π", "´©˙π")
+]
+
+
+def test_set_up():
+    """Set up server for testing purposes."""
+    from server import server
+    server()
+
+
+def test_tear_down():
+    """Tear down testing server."""
+    from server import server
+    server.shutdown(1)
+    server.close()
+
+
+@pytest.mark.parametrize('input, output', TEST_SHORT_BUFFERS)
+def test_short_buffers(input, output):
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    from client import client
+    test_set_up()
+    assert client(input) == output
+    test_tear_down()
+
+
+@pytest.mark.parametrize('input, output', TEST_LONG_BUFFERS)
+def test_long_buffers(input, output):
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    from client import client
+    test_set_up()
+    assert client(input) == output
+    test_tear_down()
+
+
+@pytest.mark.parametrize('input, output', TEST_BUFFER_MULTIPLES)
+def test_buffer_multiples(input, output):
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    from client import client
+    test_set_up()
+    assert client(input) == output
+    test_tear_down()
+
+
+@pytest.mark.parametrize('input, output', TEST_NON_ASCII)
+def test_non_ascii(input, output):
+    """Summary.
+
+    Returns:
+        TYPE: Description
+    """
+    from client import client
+    test_set_up()
+    assert client(input) == output
+    test_tear_down()
