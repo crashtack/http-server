74865311ea0d2f4fc8c57d158ebb58cda60afd4e
crashtack <crashtack@gmail.com>
2016-08-18 09:08:06 -0700

    added parse_request() and decode() decode() will be moved into parse_request() for assingnment

--- src/server.py
+++ src/server.py
@@ -29,17 +29,44 @@ def server():
         conn.close()
 
 
+def parse_request():
+    pass
+
+
+def decode(response):
+    # casts byte string responce to UNICODE
+    uresponse = response.decode('utf8')
+    # uresponse.split(CRLF)
+    CRLF = '\r\n'
+    first_pass = uresponse.split(CRLF + CRLF, 1)    # '1' splits only once
+
+    head, body = first_pass
+    head_lines = head.split(CRLF)
+
+    proto, status = head_lines[0].split(maxsplit=1)
+    headers = head_lines[1:]
+
+    headers_split = [header.split(':', 1) for header in headers]
+
+    headers_dict = {k.lower():v.strip() for k, v headers in headers_split}
+
+
+
+assert 'content-length' in headers_dict
+assert int(headers_dict['content-length']) =  len(body.encode('utf8'))
+
+
 def response_ok():
     """Function returns an HTTP '200 OK' response."""
     response = (b"HTTP/1.1 200 OK\r\nHost: 127.0.0.1:5000"
-    b"\r\n\r\nConnection Successful")
+                b"\r\n\r\nConnection Successful")
     return response
 
 
 def response_error():
     """Function returns an HTTP '500 Internal Server Error' response."""
     response = (b"HTTP/1.1 500 Internal Server Error\r\n"
-    b"Host: 127.0.0.1:5000\r\n\r\nServer Error")
+                b"Host: 127.0.0.1:5000\r\n\r\nServer Error")
     return response
 
 
